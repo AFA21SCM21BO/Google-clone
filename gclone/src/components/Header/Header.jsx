315c3c3dbd03d168dd2efefc11c397b9ac331146
AFA21SCM21BO <abonageri@hawk.iit.edu>
2022-01-17 11:57:48 -0600

    google api added and tested

--- gclone/src/components/Header/Header.jsx
+++ gclone/src/components/Header/Header.jsx
@@ -1,7 +1,27 @@
-import Box from '@mui/material/Box';
+
 import TextField from '@mui/material/TextField';
 
+
 const Header = () => {
+
+    var axios = require("axios").default;
+
+    var options = {
+      method: 'GET',
+      url: 'https://google-search1.p.rapidapi.com/google-search',
+      params: {hl: 'en', q: 'abhishek+bonageri', gl: 'us'},
+      headers: {
+        'x-rapidapi-host': 'google-search1.p.rapidapi.com',
+        'x-rapidapi-key': 'aee0e6b517msh087d45f5420df8ap1222fbjsn32b0a1ff8446'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+    
     return (
         <>
         <form  className="search">
